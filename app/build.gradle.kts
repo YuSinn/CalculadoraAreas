4fa43e9bcb8012e1f465edb73a743c9f63acb2cb
Techie4 <vladdutu8@gmail.com>
2023-11-17 10:47:14 +0100

    Second

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.calculadoraareas"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.calculadoraareas"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
