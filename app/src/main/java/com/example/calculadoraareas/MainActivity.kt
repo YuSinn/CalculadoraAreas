39556ecf4ad52f0d5c7c87b2eddd0051f875c2a9
Techie4 <vladdutu8@gmail.com>
2023-11-17 12:04:58 +0100

    third

--- app/src/main/java/com/example/calculadoraareas/MainActivity.kt
+++ app/src/main/java/com/example/calculadoraareas/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.calculadoraareas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import com.bumptech.glide.Glide
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val imageView: ImageView = findViewById(R.id.titulo)
+        Glide.with(this).asGif().load(R.drawable.titulo).into(imageView)
+
     }
 }
