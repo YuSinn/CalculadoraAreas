d0ba85435e9a1f2a65ac063f54980064c49612bc
Vladut Gabriel Alexandru <alexandru.vladut.gabriel97@gmail.com>
2023-11-18 11:16:01 +0100

    codigo repetitivo eliminado

--- app/src/main/java/com/example/calculadoraareas/MainActivity.kt
+++ app/src/main/java/com/example/calculadoraareas/MainActivity.kt
@@ -11,31 +11,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val imageView: ImageView = findViewById(R.id.titulo)
-        val imageButtonCalcular : ImageButton = findViewById(R.id.btnCalcular)
-        val imageButtonBorrar : ImageButton = findViewById(R.id.btnBorrar)
-        val imageButtonCero : ImageButton = findViewById(R.id.btnCero)
-        val imageButtonUno : ImageButton = findViewById(R.id.btnUno)
-        val imageButtonDos : ImageButton = findViewById(R.id.btnDos)
-        val imageButtonTres : ImageButton = findViewById(R.id.btnTres)
-        val imageButtonCuatro : ImageButton = findViewById(R.id.btnCuatro)
-        val imageButtonCinco : ImageButton = findViewById(R.id.btnCinco)
-        val imageButtonSeis : ImageButton = findViewById(R.id.btnSeis)
-        val imageButtonSiete : ImageButton = findViewById(R.id.btnSiete)
-        val imageButtonOcho : ImageButton = findViewById(R.id.btnOcho)
-        val imageButtonNueve : ImageButton = findViewById(R.id.btnNueve)
+
+// Lista de ImageButton y sus correspondientes recursos
+        val imageButtons: List<Pair<ImageButton, Int>> = listOf(
+            Pair(findViewById(R.id.btnCalcular), R.drawable.calcular3),
+            Pair(findViewById(R.id.btnBorrar), R.drawable.borrar2),
+            Pair(findViewById(R.id.btnCero), R.drawable.cero3),
+            Pair(findViewById(R.id.btnUno), R.drawable.uno2),
+            Pair(findViewById(R.id.btnDos), R.drawable.dos2),
+            Pair(findViewById(R.id.btnTres), R.drawable.tres2),
+            Pair(findViewById(R.id.btnCuatro), R.drawable.cuatro2),
+            Pair(findViewById(R.id.btnCinco), R.drawable.cinco2),
+            Pair(findViewById(R.id.btnSeis), R.drawable.seis2),
+            Pair(findViewById(R.id.btnSiete), R.drawable.siete2),
+            Pair(findViewById(R.id.btnOcho), R.drawable.ocho2),
+            Pair(findViewById(R.id.btnNueve), R.drawable.nueve2)
+        )
+
+// Cargar la imagen del título
         Glide.with(this).asGif().load(R.drawable.titulo7).into(imageView)
-        Glide.with(this).asGif().load(R.drawable.calcular3).into(imageButtonCalcular)
-        Glide.with(this).asGif().load(R.drawable.borrar2).into(imageButtonBorrar)
-        Glide.with(this).asGif().load(R.drawable.cero3).into(imageButtonCero)
-        Glide.with(this).asGif().load(R.drawable.uno2).into(imageButtonUno)
-        Glide.with(this).asGif().load(R.drawable.dos2).into(imageButtonDos)
-        Glide.with(this).asGif().load(R.drawable.tres2).into(imageButtonTres)
-        Glide.with(this).asGif().load(R.drawable.cuatro2).into(imageButtonCuatro)
-        Glide.with(this).asGif().load(R.drawable.cinco2).into(imageButtonCinco)
-        Glide.with(this).asGif().load(R.drawable.seis2).into(imageButtonSeis)
-        Glide.with(this).asGif().load(R.drawable.siete2).into(imageButtonSiete)
-        Glide.with(this).asGif().load(R.drawable.ocho2).into(imageButtonOcho)
-        Glide.with(this).asGif().load(R.drawable.nueve2).into(imageButtonNueve)
+
+// Cargar imágenes en los ImageButtons usando un bucle
+        for ((button, resource) in imageButtons) {
+            Glide.with(this).asGif().load(resource).into(button)
+        }
 
 
     }
